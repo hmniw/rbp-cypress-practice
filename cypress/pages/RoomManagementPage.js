61f34236d525e35b1482c9114490aee13257dec2
Will <willherring1@gmail.com>
2021-11-01 22:32:59 +0000

    [WH] Added enum for room types, and gave an initial stab at a function for creating a room

--- cypress/pages/RoomManagementPage.js
+++ cypress/pages/RoomManagementPage.js
@@ -2,11 +2,29 @@ const roomNumberInput = '#roomNumber';
 const roomTypeDropdown = '#type';
 const accessibleDropdown = '#accessible';
 const roomPriceInput = '#roomPrice';
+const createRoomButton = '#createRoom';
+const roomsList = "[data-type='room']";
+
+// Checkbox locators
 const wifiCheckbox = '#wifiCheckbox';
 const tvCheckbox = '#tvCheckbox';
 const radioCheckbox = '#radioCheckbox';
-
+const refreshmentsCheckbox = '#refreshCheckbox';
+const safeCheckbox = '#safeCheckbox';
+const viewsCheckbox = '#viewsCheckbox';
 
 class RoomManagementPage {
-
+    createRoom(roomNumber, roomType, accessible, roomPrice, checkboxes) {
+        return cy.get(roomNumberInput).type(roomNumber).then(() => {
+            cy.get(roomTypeDropdown).select(roomType);
+            cy.get(accessibleDropdown).select(accessible);
+            cy.get(roomPriceInput).type(roomPrice);
+            checkboxes.forEach((checkbox) => {
+                if (checkbox) {
+                    cy.get(checkbox).click();
+                }
+            })
+            cy.get(createRoomButton).click();
+        })
+    }
 }
---
+++ cypress/utilities/RoomTypeEnum.js
@@ -0,0 +1,7 @@
+export const roomType = {
+    SINGLE: "Single",
+    TWIN: "Twin",
+    DOUBLE: "Double",
+    FAMILY: "Family",
+    SUITE: "Suite"
+}
